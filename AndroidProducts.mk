cea1c7959d6ed32dbea960ddee72a00d59b8a6e5
Qui Huynh <deathbook1999@gmail.com>
2014-07-15 11:49:23 +0700

    AOSPA for codinap
    Here we go GT-I8160P full support

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,4 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_codinap.mk
+ifeq (pa_codinap,$(TARGET_PRODUCT))
+		PRODUCT_MAKEFILES += $(LOCAL_DIR)/full_codinap.mk \
+		$(LOCAL_DIR)/pa_codinap.mk
+endif
